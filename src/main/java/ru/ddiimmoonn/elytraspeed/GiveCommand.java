9549e7f42d41877862a576ba5c65e65a6305b988
DDiimmoonn <133042068+DDiimmoonn@users.noreply.github.com>
2026-07-29 04:26:48 +0400

    Refactor GiveCommand to simplify item giving logic

--- src/main/java/ru/ddiimmoonn/elytraspeed/GiveCommand.java
+++ src/main/java/ru/ddiimmoonn/elytraspeed/GiveCommand.java
@@ -1,47 +1,34 @@
 package ru.ddiimmoonn.elytraspeed;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.ChatColor;
+import org.bukkit.inventory.ItemStack;
 
 public class GiveCommand implements CommandExecutor {
-    private final Plugin plugin;
-    public GiveCommand(Plugin plugin) { this.plugin = plugin; }
+    private final ElytraSpeedPlugin plugin;
+
+    public GiveCommand(ElytraSpeedPlugin plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!sender.hasPermission("elytraspeed.give") && !(sender instanceof ConsoleCommandSender)) {
-            sender.sendMessage(ChatColor.RED + "У вас нет прав.");
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command is for players only.");
             return true;
         }
-        if (args.length < 2) {
-            sender.sendMessage(ChatColor.YELLOW + "Использование: /gelytra <ник> <уровень 1-5>");
-            return true;
+        Player player = (Player) sender;
+        int level = 1;
+        if (args.length > 0) {
+            try {
+                level = Math.max(1, Integer.parseInt(args[0]));
+            } catch (NumberFormatException ignored) {}
         }
-        Player target = Bukkit.getPlayerExact(args[0]);
-        if (target == null) {
-            sender.sendMessage(ChatColor.RED + "Игрок не найден.");
-            return true;
-        }
-        int lvl;
-        try {
-            lvl = Integer.parseInt(args[1]);
-        } catch (NumberFormatException ex) {
-            sender.sendMessage(ChatColor.RED + "Неверный уровень.");
-            return true;
-        }
-        if (lvl < 1) lvl = 1;
-        if (lvl > 5) lvl = 5;
-        target.getInventory().addItem(ItemUtils.createElytraWithLevel(lvl, plugin));
-        sender.sendMessage(ChatColor.GREEN + "Выдал элитру уровня " + lvl + " игроку " + target.getName());
-        if (!(sender instanceof ConsoleCommandSender)) {
-            sender.sendMessage(ChatColor.GRAY + "Готово.");
-        }
+        ItemStack el = ItemUtils.createElytra(level);
+        player.getInventory().addItem(el);
+        player.sendMessage("Given custom Elytra (level " + level + ").");
         return true;
     }
 }
