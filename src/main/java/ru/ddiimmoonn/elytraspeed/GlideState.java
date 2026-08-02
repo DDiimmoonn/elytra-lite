a198b9fe751ac4b515083f28a450cbd111012029
DDiimmoonn <133042068+DDiimmoonn@users.noreply.github.com>
2026-08-02 16:16:49 +0400

    Update GlideState.java

--- src/main/java/ru/ddiimmoonn/elytraspeed/GlideState.java
+++ src/main/java/ru/ddiimmoonn/elytraspeed/GlideState.java
@@ -5,11 +5,13 @@ public class GlideState {
     public double multiplier;
     public double dirX;
     public double dirZ;
+    public double currentSpeed;
 
-    public GlideState(double baseSpeed, double multiplier, double dirX, double dirZ) {
+    public GlideState(double baseSpeed, double multiplier, double dirX, double dirZ, double currentSpeed) {
         this.baseSpeed = baseSpeed;
         this.multiplier = multiplier;
         this.dirX = dirX;
         this.dirZ = dirZ;
+        this.currentSpeed = currentSpeed;
     }
 }
